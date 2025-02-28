cf81be90d754f9e3b4f88ea4f48ea9cc8c7395c9
Salekin Shahir <salekinshahir789@gmail.com>
2025-03-01 01:56:31 +0600

    second comment picture dynamic

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const ringButton = document.querySelectorAll(".ring-button");
 
 for (let i = 0; i < ringButton.length; i++){
     const ringBtn = ringButton [i];
-    ringBtn.addEventListener('click', function(){
-        console.log("clicked");
+    ringBtn.addEventListener('click', function(event){
+        const color = event.target.id.replace("-color","");
+        console.log(color);
+        // color add korar aghe purple color aghe thke thakle take remove korchi...
+        for(let j = 0; j < ringButton.length; j++){
+            ringButton[j].classList.remove("border-purple-600");
+        }
+        // color add korchi.....
+      event.target.classList.add("border-purple-600");
+
+      const productImage =document.getElementById("product-image");
+      productImage.src = "../images/"+ color +".png";
     })
 }
